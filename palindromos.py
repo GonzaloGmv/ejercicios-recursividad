122738838e1a95acb76c670e1f22c67e2fbda256
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2022-02-26 20:06:21 +0100

    palindromos

--- palindromos.py
+++ palindromos.py
@@ -1,8 +1,29 @@
 import unicodedata
+
+#Filtrar la frase
 frase = input("Escriba la frase que quiere comprobar si es palindromo: ")
-frase = frase.split()
-frase = ''.join(frase)
+filtrada = frase.split()
+filtrada = ''.join(filtrada)
 trans_tab = dict.fromkeys(map(ord, u'\u0301\u0308'), None)
-frase = unicodedata.normalize('NFKC', unicodedata.normalize('NFKD', frase).translate(trans_tab))
-frase = frase.lower()
-print(frase)
+filtrada = unicodedata.normalize('NFKC', unicodedata.normalize('NFKD', filtrada).translate(trans_tab))
+filtrada = filtrada.lower()
+
+#Función parea reconocer palindromos
+def palindromo(N, M):
+    if N < M:
+        if filtrada[N] == filtrada[M]:
+            palindromo(N+1, M-1)
+        else:
+            print(frase, "no es palindromo")
+    elif N > M:
+        palindromo(N, M+1)
+    else:
+        if filtrada[N] == filtrada[M]:
+            print(frase, "es palindromo")
+        else:
+            print(frase, "no es palindromo")
+
+#Ejecucion del ejercicio
+m = len(filtrada)
+n = 0
+palindromo(n, m-1)
